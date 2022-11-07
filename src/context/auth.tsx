205072c94d02e9610e1b38f34df0554d79395373
nguyend-nam <nguyennamnade22@gmail.com>
2022-11-07 08:39:50 +0700

    chore: add auth_token_key

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,8 +1,7 @@
-import { createContext } from '@dwarvesf/react-utils'
+import { createContext, isSSR } from '@dwarvesf/react-utils'
 import { Session } from 'next-auth'
 import { useSession, signIn, signOut } from 'next-auth/react'
 import { useEffect, useState } from 'react'
-// import { useCallback, useState } from 'react'
 import { WithChildren } from 'types/common'
 
 interface AuthContextValues {
@@ -12,6 +11,8 @@ interface AuthContextValues {
   session: Session | null
 }
 
+const AUTH_TOKEN_KEY = 'fortress-token'
+
 const [Provider, useAuthContext] = createContext<AuthContextValues>({
   name: 'auth',
 })
@@ -19,11 +20,17 @@ const [Provider, useAuthContext] = createContext<AuthContextValues>({
 const AuthContextProvider = ({ children }: WithChildren) => {
   const { data: session, status } = useSession()
 
-  const [isLogin, setIsLogin] = useState<boolean>(false)
+  const [isLogin, setIsLogin] = useState<boolean>(() => {
+    return isSSR()
+      ? false
+      : Boolean(window.localStorage.getItem(AUTH_TOKEN_KEY))
+  })
 
   useEffect(() => {
     setIsLogin(status === 'authenticated' && session !== null)
   }, [session, status])
+
+  // TODO: bind API and implement login and logout functions with localStorage's setItem and removeItem
 
   return (
     <Provider
