d6f3f8953d1d1c30194805a9d27668352423c6b3
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2022-11-10 09:56:39 +0700

    feat: header profile dropdown

--- src/pages/config/index.tsx
+++ src/pages/config/index.tsx
@@ -1,17 +1,5 @@
-import { Button, Typography } from 'antd'
-import { useAuthContext } from 'context/auth'
-
 const Default = () => {
-  const { logout, session } = useAuthContext()
-  const { Text } = Typography
-
-  return (
-    <>
-      <Text>{session?.user?.name}</Text>
-      <br />
-      <Button onClick={() => logout()}>Logout</Button>
-    </>
-  )
+  return <>Config</>
 }
 
 export default Default
