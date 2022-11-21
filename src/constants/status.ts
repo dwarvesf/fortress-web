aeb30e6142ebb39b49b303b464c640fed7d20ff5
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2022-11-21 14:37:41 +0700

    feat: bind update employee status api (#20)

--- src/constants/status.ts
+++ src/constants/status.ts
@@ -1,4 +1,11 @@
-export const employeeStatuses = {
+export type EmployeeStatus =
+  | 'left'
+  | 'on-boarding'
+  | 'probation'
+  | 'full-time'
+  | 'contractor'
+
+export const employeeStatuses: Record<EmployeeStatus, string> = {
   left: 'Left',
   'on-boarding': 'On Boarding',
   probation: 'Probation',
