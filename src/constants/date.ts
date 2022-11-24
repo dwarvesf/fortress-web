d303255354386caf92d656cca86d288e27143c1c
nguyend-nam <nguyennamnade22@gmail.com>
2022-11-24 10:24:32 +0700

    chore: remove unused date format

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,4 +1,3 @@
 export const DATE_FORMAT = 'dd-MM-yyyy'
 export const SERVER_DATE_FORMAT = 'yyyy-MM-dd'
 export const SELECT_BOX_DATE_FORMAT = 'DD-MM-yyyy'
-export const CREATE_PROJECT_DATE_FORMAT = 'yyyy-MM-DD'
