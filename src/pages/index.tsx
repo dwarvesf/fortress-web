76128783fc1b11114f76038277404afd5261e7c4
nguyend-nam <nguyennamnade22@gmail.com>
2022-12-15 16:32:19 +0700

    feat: route to projects as default

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,14 @@
+import { ROUTES } from 'constants/routes'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 const DashboardPage = () => {
+  const { push } = useRouter()
+
+  useEffect(() => {
+    push(ROUTES.PROJECTS)
+  }, [push])
+
   return <>Dashboard</>
 }
 
