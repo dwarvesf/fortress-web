c2a21971c4cce9e736115c22a1518d3ee064eefc
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2022-12-15 13:43:22 +0700

    feat: mock ui create engagement survey (#71)

---
+++ src/components/pages/feedbacks/engagement/CreateEngagementSurveyModal.tsx
@@ -0,0 +1,79 @@
+import { Form, Modal, notification, Select } from 'antd'
+import { useForm } from 'antd/lib/form/Form'
+import { useState } from 'react'
+
+interface Props {
+  isOpen: boolean
+  initialValues: Partial<any> // TODO: add type
+  onClose: () => void
+  onAfterSubmit: () => void
+}
+
+export const CreateEngagementSurveyModal = (props: Props) => {
+  const { isOpen, initialValues, onClose, onAfterSubmit } = props
+  const [form] = useForm()
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const currentYear = new Date().getFullYear()
+
+  const onSubmit = async () => {
+    try {
+      setIsSubmitting(true)
+
+      notification.success({
+        message: 'Employee engagement survey event created successfully!',
+      })
+
+      onClose()
+      onAfterSubmit()
+    } catch (error: any) {
+      notification.error({
+        message:
+          error?.message ||
+          'Could not create employee engagement survey event!',
+      })
+    } finally {
+      setIsSubmitting(false)
+    }
+  }
+
+  return (
+    <Modal
+      open={isOpen}
+      onCancel={onClose}
+      onOk={form.submit}
+      okText="Create"
+      okButtonProps={{ loading: isSubmitting }}
+      destroyOnClose
+      title="New event"
+    >
+      <Form form={form} onFinish={onSubmit} initialValues={initialValues}>
+        <Form.Item
+          label="Quarter"
+          name="quarter"
+          rules={[{ required: true, message: 'Please select quarter' }]}
+        >
+          <Select
+            placeholder="Select quarter"
+            options={['q1', 'q2', 'q3', 'q4'].map((value) => ({
+              label: value.toUpperCase(),
+              value,
+            }))}
+          />
+        </Form.Item>
+        <Form.Item
+          label="Year"
+          name="year"
+          rules={[{ required: true, message: 'Please select year' }]}
+        >
+          <Select
+            placeholder="Select year"
+            options={[...Array(10).keys()].map((i) => ({
+              label: currentYear + i,
+              value: currentYear + i,
+            }))}
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  )
+}
