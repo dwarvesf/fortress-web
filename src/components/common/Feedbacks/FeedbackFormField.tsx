c7969d410d30d505852f4403be5a6757aa29cb24
Hien Le <leduyhien152@gmail.com>
2022-12-19 16:20:54 +0700

    feat: view & fill engagement survey api

--- src/components/common/Feedbacks/FeedbackFormField.tsx
+++ src/components/common/Feedbacks/FeedbackFormField.tsx
@@ -32,7 +32,7 @@ export const FeedbackFormField = (props: Props) => {
           rules={[{ required, message: 'Required' }]}
         >
           {/* @ts-ignore */}
-          <TextArea {...rest} rows={3} bordered readOnly={disabled} />
+          <TextArea {...rest} rows={3} bordered readOnly={done} />
         </Form.Item>
       )
     }
