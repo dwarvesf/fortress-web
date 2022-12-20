fc6d85ae4b8b8fb63b628bfebaf329bbbe636e45
nguyend-nam <nguyennamnade22@gmail.com>
2022-12-20 11:11:36 +0700

    feat: mock ui view workload result

--- src/components/pages/feedbacks/work/WorkDetailActions.tsx
+++ src/components/pages/feedbacks/work/WorkDetailActions.tsx
@@ -1,6 +1,8 @@
+import { useDisclosure } from '@dwarvesf/react-hooks'
 import { PreviewOpen } from '@icon-park/react'
 import { Row, Tooltip } from 'antd'
 import { Button } from 'components/common/Button'
+import { WorkloadResultModal } from './WorkloadResultModal'
 
 interface Props {
   record: any
@@ -9,16 +11,30 @@ interface Props {
 export const WorkDetailActions = (props: Props) => {
   const { record } = props
 
+  const {
+    isOpen: isWorkloadResultDialogOpen,
+    onOpen: openWorkloadResultDialog,
+    onClose: closeWorkloadResultDialog,
+  } = useDisclosure()
+
   return (
-    <Row justify="end" gutter={[8, 8]}>
-      <Tooltip title="View">
-        <Button
-          type="text-primary"
-          size="small"
-          icon={<PreviewOpen size={20} />}
-          onClick={() => console.log(record)}
-        />
-      </Tooltip>
-    </Row>
+    <>
+      <Row justify="end" gutter={[8, 8]}>
+        <Tooltip title="View">
+          <Button
+            type="text-primary"
+            size="small"
+            icon={<PreviewOpen size={20} />}
+            onClick={openWorkloadResultDialog}
+          />
+        </Tooltip>
+      </Row>
+
+      <WorkloadResultModal
+        onClose={closeWorkloadResultDialog}
+        isOpen={isWorkloadResultDialogOpen}
+        record={record}
+      />
+    </>
   )
 }
