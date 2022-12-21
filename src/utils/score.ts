cc66ea86f857ad47e1d22b624e55531b5bd3cf76
nguyend-nam <nguyennamnade22@gmail.com>
2022-12-21 18:13:08 +0700

    chore: update naming

--- src/utils/score.ts
+++ src/utils/score.ts
@@ -1,20 +1,20 @@
 import { AgreementLevel } from 'constants/agreementLevel'
 import { ViewDomain } from 'types/schema'
 
-export const mapScoreToLikertScale = (model: ViewDomain): AgreementLevel => {
-  if (!model.average) {
+export const mapScoreToLikertScale = (record: ViewDomain): AgreementLevel => {
+  if (!record || !record.average) {
     return AgreementLevel.STRONGLY_DISAGREE
   }
-  if (model?.average <= 1.5) {
+  if (record?.average <= 1.5) {
     return AgreementLevel.STRONGLY_DISAGREE
   }
-  if (model?.average <= 2.5) {
+  if (record?.average <= 2.5) {
     return AgreementLevel.DISAGREE
   }
-  if (model?.average <= 3.5) {
+  if (record?.average <= 3.5) {
     return AgreementLevel.MIXED
   }
-  if (model?.average <= 4.5) {
+  if (record?.average <= 4.5) {
     return AgreementLevel.AGREE
   }
   return AgreementLevel.STRONGLY_AGREE
