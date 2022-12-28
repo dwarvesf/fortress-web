8ada9fec91e5d3e67481bfebf760fb4bd7ec8ad8
Ngo Lap Nguyen <ngolap.nguyen@gmail.com>
2022-12-28 11:41:57 +0700

    fix: display date format from dd-MM-yyyy to dd/MM/yyyy

--- src/constants/date.ts
+++ src/constants/date.ts
@@ -1,4 +1,4 @@
-export const DATE_FORMAT = 'dd-MM-yyyy'
-export const DATETIME_FORMAT = 'dd-MM-yyyy HH:mm:ss'
+export const DATE_FORMAT = 'dd/MM/yyyy'
+export const DATETIME_FORMAT = 'dd/MM/yyyy HH:mm:ss'
 export const SERVER_DATE_FORMAT = 'yyyy-MM-dd'
-export const SELECT_BOX_DATE_FORMAT = 'DD-MM-yyyy'
+export const SELECT_BOX_DATE_FORMAT = 'DD/MM/yyyy'
