49377a83a14d3f6093404ed1cf9751b6854d0281
Nam Nguyen Dinh <nguyennamnade22@gmail.com>
2023-01-09 14:28:39 +0700

    feat: mock ui for project size on dashboard (#125)

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,6 +3,7 @@ import { Breadcrumb } from 'components/common/Header/Breadcrumb'
 import { PageHeader } from 'components/common/PageHeader'
 import { SEO } from 'components/common/SEO'
 import Engagement from 'components/pages/dashboard/engagement/Engagement'
+import Projects from 'components/pages/dashboard/projects/Projects'
 import { useState } from 'react'
 
 // mock interface, this should be base on the filter (department, seniority,...) and provided by BE
@@ -12,173 +13,26 @@ interface Feedbacks {
   engineering?: number
 }
 
-interface Dataset {
-  name?: string
-  average?: number
-  feedbacks?: Feedbacks
-}
-
 export interface EngagementAverageProps {
   question?: string
-  dataset?: Dataset[]
+  dataset?: {
+    name?: string
+    average?: number
+    feedbacks?: Feedbacks
+  }[]
 }
 
-export const mockData: EngagementAverageProps[] = [
-  {
-    question: 'Do I know what is expected of me at work?',
-    dataset: [
-      {
-        name: 'Q1/2022',
-        average: 2.4,
-        feedbacks: {
-          design: 3,
-          operation: 2,
-          engineering: 2.2,
-        },
-      },
-      {
-        name: 'Q2/2022',
-        average: 4,
-        feedbacks: {
-          design: 3,
-          operation: 4.5,
-          engineering: 4.5,
-        },
-      },
-      {
-        name: 'Q3/2022',
-        average: 3.5,
-        feedbacks: {
-          design: 4,
-          operation: 3.1,
-          engineering: 3.4,
-        },
-      },
-      {
-        name: 'Q4/2022',
-        average: 5,
-        feedbacks: {
-          design: 5,
-          operation: 5,
-          engineering: 5,
-        },
-      },
-      {
-        name: 'Q1/2023',
-        average: 4.5,
-        feedbacks: {
-          design: 3.5,
-          operation: 5,
-          engineering: 5,
-        },
-      },
-    ],
-  },
-  {
-    question:
-      'Do I have the materials and equipment I need to do my work right?',
-    dataset: [
-      {
-        name: 'Q1/2022',
-        average: 2.4,
-        feedbacks: {
-          design: 3,
-          operation: 2,
-          engineering: 2.2,
-        },
-      },
-      {
-        name: 'Q2/2022',
-        average: 2,
-        feedbacks: {
-          design: 1.5,
-          operation: 2.5,
-          engineering: 2,
-        },
-      },
-      {
-        name: 'Q3/2022',
-        average: 3.2,
-        feedbacks: {
-          design: 4,
-          operation: 2.2,
-          engineering: 3.4,
-        },
-      },
-      {
-        name: 'Q4/2022',
-        average: 3,
-        feedbacks: {
-          design: 3,
-          operation: 2.7,
-          engineering: 3.3,
-        },
-      },
-      {
-        name: 'Q1/2023',
-        average: 5,
-        feedbacks: {
-          design: 5,
-          operation: 5,
-          engineering: 5,
-        },
-      },
-    ],
-  },
-  {
-    question: 'Do I have the opportunity to do what I do best every day?',
-    dataset: [
-      {
-        name: 'Q1/2022',
-        average: 2.4,
-        feedbacks: {
-          design: 3,
-          operation: 2,
-          engineering: 2.2,
-        },
-      },
-      {
-        name: 'Q2/2022',
-        average: 3,
-        feedbacks: {
-          design: 3,
-          operation: 3.5,
-          engineering: 2.5,
-        },
-      },
-      {
-        name: 'Q3/2022',
-        average: 4,
-        feedbacks: {
-          design: 4,
-          operation: 4.1,
-          engineering: 3.9,
-        },
-      },
-      {
-        name: 'Q4/2022',
-        average: 5,
-        feedbacks: {
-          design: 5,
-          operation: 5,
-          engineering: 5,
-        },
-      },
-      {
-        name: 'Q1/2023',
-        average: 0,
-        feedbacks: {
-          design: 0,
-          operation: 0,
-          engineering: 0,
-        },
-      },
-    ],
-  },
-]
+export interface ProjectSizeProps {
+  dataset?: {
+    id: string
+    name?: string
+    value?: number
+  }[]
+}
 
 const DashboardPage = () => {
   const [filterCategory, setFilterCategory] = useState<string>('department')
+  const [currentTab, setCurrentTab] = useState<string>('projects')
 
   return (
     <>
@@ -196,27 +50,29 @@ const DashboardPage = () => {
 
       <Space direction="vertical" size={24} style={{ width: '100%' }}>
         <Tabs
-          defaultActiveKey="engagement"
-          onTabClick={() => {}}
+          defaultActiveKey={currentTab}
+          onTabClick={(t) => setCurrentTab(t)}
           tabBarExtraContent={
-            <Select
-              style={{ width: 135 }}
-              value={filterCategory}
-              onChange={setFilterCategory}
-              options={[
-                { label: 'Department', value: 'department' },
-                { label: 'Chapter', value: 'chapter' },
-                { label: 'Seniority', value: 'seniority' },
-                { label: 'Project', value: 'project' },
-              ]}
-            />
+            currentTab === 'engagement' ? (
+              <Select
+                style={{ width: 135 }}
+                value={filterCategory}
+                onChange={setFilterCategory}
+                options={[
+                  { label: 'Department', value: 'department' },
+                  { label: 'Chapter', value: 'chapter' },
+                  { label: 'Seniority', value: 'seniority' },
+                  { label: 'Project', value: 'project' },
+                ]}
+              />
+            ) : null
           }
           items={[
-            // {
-            //   key: 'projects',
-            //   label: `Projects`,
-            //   children: <>Projects</>,
-            // },
+            {
+              key: 'projects',
+              label: `Projects`,
+              children: <Projects />,
+            },
             // {
             //   key: 'resources',
             //   label: `Resources`,
