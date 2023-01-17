3b595a3bd9e031b808a88adddc0ee23f537a21e3
Hien Le <44285614+leduyhien152@users.noreply.github.com>
2023-01-17 09:45:49 +0700

    feat: handle 403 and 404 request (#145)

---
+++ src/components/common/ErrorBoundary/index.tsx
@@ -0,0 +1,34 @@
+import { GET_PATHS } from 'libs/apis'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { SWRConfig } from 'swr'
+import { WithChildren } from 'types/common'
+
+const parseKey = (value: string) => {
+  return ['@', JSON.stringify(value)].join('')
+}
+
+const ErrorBoundary = ({ children }: WithChildren) => {
+  const { replace } = useRouter()
+
+  return (
+    <SWRConfig
+      value={{
+        onError: (error, key) => {
+          if (![403, 404].includes(error?.response?.status)) return
+          if (
+            [GET_PATHS.getProjects, GET_PATHS.getEmployees].some((path) =>
+              key.startsWith(parseKey(path)),
+            )
+          ) {
+            replace('/404')
+          }
+        },
+      }}
+    >
+      {children}
+    </SWRConfig>
+  )
+}
+
+export default ErrorBoundary
