f55b74402514cfdaf4832df69e1127aacdeb8af4
Hien Le <44285614+leduyhien152@users.noreply.github.com>
2023-01-17 15:20:38 +0700

    fix: update social links (#148)

--- src/components/pages/profile/EditProfileInfoModal.tsx
+++ src/components/pages/profile/EditProfileInfoModal.tsx
@@ -143,6 +143,26 @@ export const EditProfileInfoModal = (props: Props) => {
               <Input placeholder="Enter personal email" className="bordered" />
             </Form.Item>
           </Col>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item label="Discord" name="discordName">
+              <Input placeholder="Enter Discord Name" className="bordered" />
+            </Form.Item>
+          </Col>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item label="Github" name="githubID">
+              <Input placeholder="Enter Github ID" className="bordered" />
+            </Form.Item>
+          </Col>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item label="Notion" name="notionName">
+              <Input placeholder="Enter Notion Name" className="bordered" />
+            </Form.Item>
+          </Col>
+          <Col span={24} md={{ span: 12 }}>
+            <Form.Item label="LinkedIn" name="linkedInName">
+              <Input placeholder="Enter LinkedIn Name" className="bordered" />
+            </Form.Item>
+          </Col>
           <Col span={24}>
             <Form.Item
               label="Address"
