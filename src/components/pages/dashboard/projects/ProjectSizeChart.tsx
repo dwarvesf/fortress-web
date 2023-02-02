5b08d74cef24cb7cba4b740c9dbf1b762ed7d6ab
nguyend-nam <nguyennamnade22@gmail.com>
2023-02-02 16:57:06 +0700

    chore: minor charts ui updates

--- src/components/pages/dashboard/projects/ProjectSizeChart.tsx
+++ src/components/pages/dashboard/projects/ProjectSizeChart.tsx
@@ -48,7 +48,7 @@ export const ProjectSizeChart = (props: Props) => {
   return (
     <PieChart
       width="100%"
-      minWidth={375}
+      minWidth={385}
       height={350}
       dataset={dataset}
       pieDataKey="size"
