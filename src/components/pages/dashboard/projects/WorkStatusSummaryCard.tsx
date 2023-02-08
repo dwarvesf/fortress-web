7b3139da73b0841472e0347ee4df678ca4797f95
nguyend-nam <nguyennamnade22@gmail.com>
2023-02-08 23:29:38 +0700

    chore: update quarterly summary table ui

--- src/components/pages/dashboard/projects/WorkStatusSummaryCard.tsx
+++ src/components/pages/dashboard/projects/WorkStatusSummaryCard.tsx
@@ -1,6 +1,8 @@
 import { Card } from 'antd'
 import Table, { ColumnsType } from 'antd/lib/table'
 import { ProjectAvatar } from 'components/common/AvatarWithName'
+import { ROUTES } from 'constants/routes'
+import { useRouter } from 'next/router'
 import { ViewAuditSummaries, ViewAuditSummary } from 'types/schema'
 import { getTrendByPercentage, getTrendStatusColor } from 'utils/score'
 
@@ -11,15 +13,21 @@ interface Props {
 
 const SummaryTdRender = ({
   value,
+  hasFloatingPoint = false,
+  reverseTrendColor = false,
 }: {
   value: { value: number; trend: number }
+  hasFloatingPoint?: boolean
+  reverseTrendColor?: boolean
 }) => (
   <div style={{ display: 'flex', alignItems: 'end' }}>
-    <span>{value.value.toFixed(1)}</span>
+    <span>{hasFloatingPoint ? value.value.toFixed(1) : value.value}</span>
     {getTrendByPercentage(value.trend) && (
       <span
         style={{
-          color: getTrendStatusColor(value.trend),
+          color: getTrendStatusColor(
+            reverseTrendColor ? -value.trend : value.trend,
+          ),
           fontSize: 13,
         }}
       >
@@ -33,6 +41,8 @@ export const WorkStatusSummaryCard = (props: Props) => {
   const { data, isLoading } = props
 
   const dataset = data.summary || []
+
+  const { push } = useRouter()
 
   const columns: ColumnsType<ViewAuditSummary> = [
     {
@@ -49,53 +59,73 @@ export const WorkStatusSummaryCard = (props: Props) => {
       sorter: (a, b) => (a.size && b.size ? a.size.value! - b.size.value! : 0),
     },
     {
-      title: 'Health score',
-      key: 'health',
-      dataIndex: 'health',
-      render: (value) => <SummaryTdRender value={value} />,
-      sorter: (a, b) =>
-        a.health && b.health ? a.health.value! - b.health.value! : 0,
+      title: 'Scores',
+      children: [
+        {
+          title: 'Health',
+          key: 'health',
+          dataIndex: 'health',
+          render: (value) => <SummaryTdRender value={value} hasFloatingPoint />,
+          sorter: (a, b) =>
+            a.health && b.health ? a.health.value! - b.health.value! : 0,
+        },
+        {
+          title: 'Audit',
+          key: 'audit',
+          dataIndex: 'audit',
+          render: (value) => <SummaryTdRender value={value} hasFloatingPoint />,
+          sorter: (a, b) =>
+            a.audit && b.audit ? a.audit.value! - b.audit.value! : 0,
+        },
+      ],
     },
     {
-      title: 'Audit score',
-      key: 'audit',
-      dataIndex: 'audit',
-      render: (value) => <SummaryTdRender value={value} />,
-      sorter: (a, b) =>
-        a.audit && b.audit ? a.audit.value! - b.audit.value! : 0,
-    },
-    {
-      title: 'New items',
-      key: 'newItem',
-      dataIndex: 'newItem',
-      render: (value) => <SummaryTdRender value={value} />,
-      sorter: (a, b) =>
-        a.newItem && b.newItem ? a.newItem.value! - b.newItem.value! : 0,
-    },
-    {
-      title: 'Resolved items',
-      key: 'resolvedItem',
-      dataIndex: 'resolvedItem',
-      render: (value) => <SummaryTdRender value={value} />,
-      sorter: (a, b) =>
-        a.resolvedItem && b.resolvedItem
-          ? a.resolvedItem.value! - b.resolvedItem.value!
-          : 0,
+      title: 'Action Items',
+      children: [
+        {
+          title: 'New',
+          key: 'newItem',
+          dataIndex: 'newItem',
+          render: (value) => (
+            <SummaryTdRender value={value} reverseTrendColor />
+          ),
+          sorter: (a, b) =>
+            a.newItem && b.newItem ? a.newItem.value! - b.newItem.value! : 0,
+        },
+        {
+          title: 'Resolved',
+          key: 'resolvedItem',
+          dataIndex: 'resolvedItem',
+          render: (value) => (
+            <SummaryTdRender value={value} reverseTrendColor />
+          ),
+          sorter: (a, b) =>
+            a.resolvedItem && b.resolvedItem
+              ? a.resolvedItem.value! - b.resolvedItem.value!
+              : 0,
+        },
+      ],
     },
   ]
   return (
     <Card
       title="Quarterly Summary"
       style={{ height: '100%' }}
-      bodyStyle={{ padding: '1px 0 24px' }}
+      bodyStyle={{ padding: '1px 0 0' }}
     >
       <Table
         dataSource={dataset}
         columns={columns}
         rowKey={(row) => row.id || ''}
         pagination={false}
-        scroll={{ x: 'max-content', y: 250 }}
+        scroll={{ x: 'max-content', y: 230 }}
         loading={isLoading}
+        onRow={(record) => ({
+          onClick: (e) => {
+            if (e.defaultPrevented) return
+            push(ROUTES.PROJECT_DETAIL(record.code!))
+          },
+        })}
       />
     </Card>
   )
