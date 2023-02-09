07b0d7c7ce508243a56b52075e56b16109c0f1ca
nguyend-nam <nguyennamnade22@gmail.com>
2023-02-09 13:33:54 +0700

    feat: fill quarters util

---
+++ src/utils/quarter.ts
@@ -0,0 +1,24 @@
+const quartersEnum = ['Q1', 'Q2', 'Q3', 'Q4']
+
+export const fillQuarters = (quarters: string[]) => {
+  if (quarters.length >= 4) {
+    return quarters
+  }
+
+  while (quarters.length < 4) {
+    const firstAppearanceYear = quarters[0].split('/')[1]
+    const firstAppearanceQuarter = quarters[0].split('/')[0]
+
+    const prevYear =
+      quartersEnum.indexOf(firstAppearanceQuarter) === 0
+        ? String(parseFloat(firstAppearanceYear) - 1)
+        : firstAppearanceYear
+
+    const prevQuarter =
+      quartersEnum[(3 + quartersEnum.indexOf(firstAppearanceQuarter)) % 4]
+
+    quarters = [`${prevQuarter}/${prevYear}`, ...quarters]
+  }
+
+  return quarters
+}
---
+++ src/utils/tests/quarter.test.ts
@@ -0,0 +1,15 @@
+import { fillQuarters } from '../quarter'
+
+test.each([
+  [['Q1/2020'], ['Q2/2019', 'Q3/2019', 'Q4/2019', 'Q1/2020']],
+  [
+    ['Q3/2022', 'Q4/2022', 'Q1/2023'],
+    ['Q2/2022', 'Q3/2022', 'Q4/2022', 'Q1/2023'],
+  ],
+  [
+    ['Q2/2022', 'Q4/2022'],
+    ['Q4/2021', 'Q1/2022', 'Q2/2022', 'Q4/2022'],
+  ],
+])('fillQuarters(%s, %s)', (quarters, expected) => {
+  expect(fillQuarters(quarters)).toStrictEqual(expected)
+})
