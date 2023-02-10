707bf45c9d8e3bd3f2d0a9a7a66544ad5b9f2f7a
Hien Le <44285614+leduyhien152@users.noreply.github.com>
2023-02-10 16:16:13 +0700

    feat: dashboard work survey summary api (#184)

--- src/components/pages/dashboard/resources/WorkSurveyResult/index.tsx
+++ src/components/pages/dashboard/resources/WorkSurveyResult/index.tsx
@@ -1,7 +1,20 @@
-import { Card, Tabs } from 'antd'
+import { useDebounce } from '@dwarvesf/react-hooks'
+import { Card, Input, Row, Space, Tabs } from 'antd'
+import { DomainTypes } from 'constants/feedbackTypes'
+import { useFetchWithCache } from 'hooks/useFetchWithCache'
+import { client, GET_PATHS } from 'libs/apis'
+import { useState } from 'react'
+import { capitalizeFirstLetter } from 'utils/string'
 import { WorkSurveyTable } from './WorkSurveyTable'
 
 export const WorkSurveyResult = () => {
+  const [value, setValue] = useState('')
+  const debouncedValue = useDebounce(value, 300)
+  const { data } = useFetchWithCache(
+    [GET_PATHS.getResourceWorkSurveySummaries, debouncedValue],
+    () => client.getResourceWorkSurveySummaries(debouncedValue),
+  )
+
   return (
     <Card
       style={{ height: 500, display: 'flex', flexDirection: 'column' }}
@@ -9,23 +22,29 @@ export const WorkSurveyResult = () => {
     >
       <Tabs
         tabBarStyle={{ padding: '0 24px', margin: 0, height: 42 }}
-        items={[
-          {
-            key: 'workload',
-            label: `Workload`,
-            children: <WorkSurveyTable domain="workload" />,
-          },
-          {
-            key: 'deadline',
-            label: `Deadline`,
-            children: <WorkSurveyTable domain="deadline" />,
-          },
-          {
-            key: 'learning',
-            label: `Learning`,
-            children: <WorkSurveyTable domain="learning" />,
-          },
-        ]}
+        items={['workload', 'deadline', 'learning'].map((key) => ({
+          key,
+          label: capitalizeFirstLetter(key),
+          children: (
+            <Space direction="vertical" style={{ width: '100%' }}>
+              <Row justify="end" style={{ padding: 8 }}>
+                <Input
+                  placeholder="Search by name"
+                  className="bordered"
+                  style={{ maxWidth: 300 }}
+                  value={value}
+                  onChange={(e) => {
+                    setValue(e.target.value)
+                  }}
+                />
+              </Row>
+              <WorkSurveyTable
+                domain={key as DomainTypes}
+                data={data?.data?.find((each) => each.type === key)}
+              />
+            </Space>
+          ),
+        }))}
       />
     </Card>
   )
