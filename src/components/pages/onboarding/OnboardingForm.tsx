c05ebeb0203ae28fb8092ea574e49fdc54ef765f
Hien Le <leduyhien152@gmail.com>
2023-05-20 22:38:19 +0700

    feat: submit onboarding form

--- src/components/pages/onboarding/OnboardingForm.tsx
+++ src/components/pages/onboarding/OnboardingForm.tsx
@@ -274,7 +274,7 @@ export const OnboardingForm = () => {
             >
               <Select placeholder="Select city" showSearch allowClear>
                 {countries
-                  .find((each) => each.id === country)
+                  .find((each) => each.code === country)
                   ?.cities?.map((key) => (
                     <Select.Option key={key} value={key} label={key}>
                       {key}
